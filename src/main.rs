1e713a96e9264e3f7465db2cbd7ae8e4430b9253
Justin <justinraymen@gmail.com>
2022-07-29 21:19:10 -0700

    Small changes to json and world selection

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate directories;
-#[macro_use]
 extern crate serde_json;
 use directories::BaseDirs;
 
 use std::env;
 use std::fs;
+use std::io;
 use std::path::{Path, PathBuf};
+use std::vec::Vec;
+
+use serde_json::Value;
 
 fn detect_os() -> &'static str {
     return env::consts::OS;
@@ -36,7 +39,71 @@ fn config_check() -> bool {
 }
 
 fn create_mirrorconfig() {
-    fs::File::create("mirrorconfig.json");
+    let _f = fs::File::create("mirrorconfig.json");
+    let _f = match _f {
+        Ok(file) => file,
+        Err(error) => panic!("Problem creating mirrorconfig: {:?}", error),
+    };
+}
+
+fn write_mirrorconfig(config: Value) {
+    let _f = fs::write("mirrorconfig.json", config.to_string());
+    let _f = match _f {
+        Ok(file) => file,
+        Err(error) => panic!("Problem writing to mirrorconfig: {:?}", error),
+    };
+}
+
+fn is_numeric(selection: String) -> bool {
+    for c in selection.chars() {
+        if !c.is_numeric() {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn verify_selection(user_input: &String) -> Vec<i8> {
+    let mut selections: Vec<i8> = Vec::new();
+    if user_input.len() == 1 {
+        if is_numeric(user_input.to_string()) {
+            println!("{}", "Is numeric!");
+            let num: i8 = user_input.parse().unwrap();
+            selections.push(num)
+        }
+    }
+    //Check length of string
+    // if 1 -> check is numeric
+        //If yes check if bigger than 0, return
+        //else panic
+    // if bigger than 1
+    return selections
+}
+
+
+fn confirm_worlds(worlds_path: &mut PathBuf) -> Vec<PathBuf> {
+    let mut worlds: Vec<PathBuf> = Vec::new();
+    let mut input = String::new();
+
+    for world in fs::read_dir(worlds_path).unwrap() {
+        worlds.push(world.unwrap().path())
+    }
+    println!("\n");
+    println!("{}", "Please enter the number(s)corresponding to which worlds you want to sync, separated by a comma. \nFor example, if you want worlds 1 2 and 3, type '1,2,3' - If you only want to sync one world, just enter a single number.");
+
+    for i in 0..worlds.len() {
+        let save: PathBuf = worlds[i].iter()
+            .skip_while(|ending| *ending !="saves")
+            .skip(1)
+            .collect();
+        println!("{}). {}", i+1, save.display());
+    }
+
+    io::stdin().read_line(&mut input).expect("Failed to get user input!");
+    let valid: Vec<i8> = verify_selection(&input);
+    println!("{:?}", valid);
+
+    return worlds;
 }
 
 fn first_time_setup() {
@@ -49,7 +116,7 @@ fn first_time_setup() {
     );
 
     push_mc_path(&mut base);
-    let game_path: &Path = base.as_path();
+    let mut game_path: PathBuf = base;
 
     if game_path.exists() {
         println!("It looks like minecraft is installed here: {:?}", game_path);
@@ -62,8 +129,14 @@ fn first_time_setup() {
         config_check(),
         "Couldn't create a config file for some reason, exiting..."
     );
+    
+    
+    //Looking for MC worlds
+    let saves_path: &mut PathBuf = &mut game_path;
+    saves_path.push("saves");
+    confirm_worlds(saves_path);
 
-    let config = json!({
+    let config = serde_json::json!({
         "config": {
             "operating_system": os,
             "custom_game_path": "false",
@@ -72,13 +145,14 @@ fn first_time_setup() {
         }
     });
 
-    fs::write("mirrorconfig.json", config.to_string());
+    write_mirrorconfig(config);
 }
+
 
 fn main() {
     if config_check() == false {
         first_time_setup();
     } else {
-        //In progress
+       first_time_setup();
     }
 }
